9f8c1b7f4d907e680629282e6024a52df8338e65
havietkok-sys <haviet.kok@gmail.com>
2025-11-04 00:51:51 +0100

    Inlogg system lite kommentarer

--- User.cs
+++ User.cs
@@ -1,10 +1,11 @@
 namespace App;
 
-class User
+public class User
 {
     public string Username;  // Användar namn
     public string Password;  // Lösen
 
+    public static readonly User Admin = new User("admin", "123");
     public User(string username, string password) // Skapar ny användare
     {
         Username = username;
